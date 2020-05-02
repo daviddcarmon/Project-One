d4e8b834fb015bd4d3c8da5785b63caa4f45836a
David Carmona <davidcarmona@gmail.com>
2020-05-02 14:33:36 -0600

    attempting to merg

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function gameStats() {
-  var queryURL = "http://dnd5eapi.co/api/";
+  var queryURL =
+    "https://www.superheroapi.com/api.php" + accessToken + dropDown;
+  var accessToken = "/10223238752664036";
+  var dropDown = "/664";
 
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    var statsResponse = "";
     console.log(response);
+    response.array.forEach((element) => {
+      console.log(element);
+    });
   });
 }
